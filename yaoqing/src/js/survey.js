e4d8587873b36b9d8205e1f0f3f77f62d127f846
余明智 <ymz@yumingzhideMac-mini.local>
2016-08-15 14:55:11 +0800

    123

---
+++ yaoqing/src/js/survey.js
@@ -0,0 +1,24 @@
+(function($,window){
+    var url = 'http://dayima.cn/ms/hotshare';//此链接内部会判断
+
+    $(".top_float").off('click').on('click',function(){
+       window.location.href= url;
+    });
+    $(".xiazai_btn").off('click').on('click',function(){
+        window.location.href= url;
+    });
+    if(navigator.userAgent.indexOf("Safari") > -1){
+        window.location.href="dayima://web/new?url=http%3a%2f%2fevent.dayima.com%2fyaoqing%2findex.html";
+    }else {
+        var dayima = new AppLink();
+        var nativeUrl="dayima://web/new?url=http%3a%2f%2fevent.dayima.com%2fyaoqing%2findex.html";
+        dayima.open({
+            error: function () {
+                // window.open(encodeURI("https://twitter.com/intent/tweet?text=Look at this awesome thing"), "_blank", "toolbar=0,personalbar=0,resizable,scrollbars,status,width=550,height=450,top=" + Math.round((screen.height - 450)/2) + ",left=" + Math.round((screen.width - 550)/2))
+            },
+            success: function () {},
+            nativeURL: encodeURI(nativeUrl)
+        })
+    }
+
+})(Zepto,window);
